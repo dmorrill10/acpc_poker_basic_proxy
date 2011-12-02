7a060709f278dcb99bf86e282e16b7c8e4fb13ad
Dustin Morrill <morrill@ualberta.ca>
2011-12-01 20:18:38 -0700

    Works now

--- src/communication_logic/matchstate_string_receiver.rb
+++ src/communication_logic/matchstate_string_receiver.rb
@@ -1,15 +1,15 @@
 
-# @todo Only want MatchstateString and AcpcPokerTypesDefs here, is there a way to select this?
+# @todo Only want MatchstateString here, is there a way to select this?
 require 'acpc_poker_types'
 
 # Receives and parses matchstate strings.
 class MatchstateStringReceiver
-   include AcpcPokerTypesDefs
    
-   # Receives a matchstate string from the given +connection+.
+   # Receives a match state string from the given +connection+.
    # @param [#gets] connection The connection from which a matchstate string should be received.
    # @return [MatchstateString] The matchstate string that was received from the +connection+.
    def self.receive_matchstate_string(connection)
-      MatchstateString.new(connection.gets)
+      raw_match_state_string = connection.gets
+      MatchstateString.new(raw_match_state_string)
    end
 end
