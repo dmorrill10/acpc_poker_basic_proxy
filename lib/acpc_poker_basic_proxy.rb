6144ed1fd4b97acce25d296741ff42b65c6039d3
Dustin Morrill <morrill@ualberta.ca>
2012-03-17 16:19:13 -0600

    Added defs to the gem's inclusion.

--- lib/acpc_poker_basic_proxy.rb
+++ lib/acpc_poker_basic_proxy.rb
@@ -1,5 +1,6 @@
 require "acpc_poker_basic_proxy/version"
 
+require File.expand_path("../../src/acpc_poker_basic_proxy_defs", __FILE__)
 require File.expand_path("../../src/basic_proxy", __FILE__)
 
 module AcpcPokerBasicProxy
