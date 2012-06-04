89cbbcfd5f80cc1c9826125a3902c8e9513a6a3b
Dustin Morrill <morrill@ualberta.ca>
2012-06-03 20:06:36 -0600

    Fixed some typos.

--- lib/acpc_poker_basic_proxy.rb
+++ lib/acpc_poker_basic_proxy.rb
@@ -1,8 +1,10 @@
-require "acpc_poker_basic_proxy/version"
+require File.expand_path("../acpc_poker_basic_proxy/version", __FILE__)
 
 require File.expand_path("../acpc_poker_basic_proxy/acpc_poker_basic_proxy_defs", __FILE__)
-require File.expand_path("../acpc_poker_basic_proxy/match_state_string_receiver", __FILE__)
-require File.expand_path("../acpc_poker_basic_proxy/action_sender", __FILE__)
+require File.expand_path("../acpc_poker_basic_proxy/communication_logic/acpc_dealer_communicator", __FILE__)
+require File.expand_path("../acpc_poker_basic_proxy/communication_logic/acpc_dealer_information", __FILE__)
+require File.expand_path("../acpc_poker_basic_proxy/communication_logic/match_state_string_receiver", __FILE__)
+require File.expand_path("../acpc_poker_basic_proxy/communication_logic/action_sender", __FILE__)
 require File.expand_path("../acpc_poker_basic_proxy/basic_proxy", __FILE__)
 
 module AcpcPokerBasicProxy
